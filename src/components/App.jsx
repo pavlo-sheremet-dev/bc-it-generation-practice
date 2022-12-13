0590aca3181b43d462950777cf72b59cf6731932
pavel-sheremet-dev <pashqa.sheremet@gmail.com>
2022-12-13 14:59:25 +0200

    save

--- src/components/App.jsx
+++
@@ -1,5 +0,0 @@
-import MyComponent from './MyComponent';
-
-export const App = () => {
-  return <MyComponent />;
-};
--- src/components/_reusable/Heading/Heading.jsx
+++
@@ -1,9 +0,0 @@
-import { HeadingStyled } from './Heading.styled';
-
-const Heading = ({ title = '', isHidden = false }) => {
-  return (
-    <HeadingStyled className={isHidden && 'isHidden'}>{title}</HeadingStyled>
-  );
-};
-
-export default Heading;
