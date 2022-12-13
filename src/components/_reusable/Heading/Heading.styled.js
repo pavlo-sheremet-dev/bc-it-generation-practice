a724b705bbf0213153477f12efa11715a87cfef9
pavel-sheremet-dev <pashqa.sheremet@gmail.com>
2022-12-13 13:33:44 +0200

    add module-1 start files

--- src/components/_reusable/Heading/Heading.styled.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const HeadingStyled = styled.h1`
-  font-size: 20px;
-  font-weight: 700;
-  font-style: normal;
-`;
--- src/components/_reusable/Section/Section.styled.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const SectionStyled = styled.section`
-  padding-top: ${({ pt }) => (pt ? pt + 'px' : '10px')};
-  padding-bottom: ${({ pb }) => (pb ? pb + 'px' : '10px')};
-  /* background-color: black; */
-  /* outline: 1px solid yellow; */
-`;
