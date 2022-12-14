d873ae47bcbc6cd7b3a4b88c899f98727a56924e
Roman Kotsiurko <kotsiurko@gmail.com>
2022-12-14 15:43:01 +0200

    solution of task 3

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -9,6 +9,7 @@ import {
 } from 'components';
 import data from 'data/article.json';
 import stats from 'data/data.json';
+import forbes from 'data/forbes.json';
 
 // test
 
@@ -38,7 +39,7 @@ export const App = () => {
         <Heading marginTop="50px" marginBottom="50px" textAlign="center">
           Task 3
         </Heading>
-        <ForbesList />
+        <ForbesList list={forbes} />
 
         <Heading marginTop="50px" marginBottom="50px" textAlign="center">
           Task 4
--- src/components/ForbesList/ForbesList.jsx
+++ src/components/ForbesList/ForbesList.jsx
@@ -7,7 +7,42 @@ import {
   TitleBottom,
   LeaderBoardProfiles,
 } from './ForbesList.styled';
+import PropTypes from 'prop-types';
 
-export const ForbesList = () => {
-  return <div>ForbesList</div>;
+export const ForbesList = ({ list }) => {
+  console.log(list);
+  return (
+    <LeaderBoard>
+      <BoardHeader>
+        <BoardTitle>
+          <TitleTop>Forbes</TitleTop>
+          <TitleBottom>Leader board</TitleBottom>
+        </BoardTitle>
+      </BoardHeader>
+
+      <LeaderBoardProfiles>
+        {list.map(({ avatar, name, capital, isIncrease, id }) => (
+          <ForbesListItem
+            key={id}
+            avatar={avatar}
+            name={name}
+            capital={capital}
+            isIncrease={isIncrease}
+          />
+        ))}
+      </LeaderBoardProfiles>
+    </LeaderBoard>
+  );
+};
+
+ForbesList.propTypes = {
+  list: PropTypes.arrayOf(
+    PropTypes.shape({
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      capital: PropTypes.number.isRequired,
+      isIncrease: PropTypes.bool.isRequired,
+      id: PropTypes.string.isRequired,
+    }).isRequired
+  ).isRequired,
 };
