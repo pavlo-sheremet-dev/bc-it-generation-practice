1437458141960c5a62b2ffed67d3116e8a4424de
pavel-sheremet-dev <pashqa.sheremet@gmail.com>
2022-12-15 13:30:32 +0200

    create branch adn load start files

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,39 +1,35 @@
-import {
-  BlogCard,
-  Container,
-  Section,
-  Heading,
-  Statistics,
-  ForbesList,
-  CryptoHistory,
-} from 'components';
-
-// test
+import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import { Container, Header, Section, Text } from 'components';
+import { Gallery, Todos } from 'tabs';
 
 export const App = () => {
   return (
-    <Section>
-      <Container>
-        <Heading marginBottom="50px" textAlign="center">
-          Task 1
-        </Heading>
-        <BlogCard />
+    <>
+      <Header />
 
-        <Heading marginTop="50px" marginBottom="50px" textAlign="center">
-          Task 2
-        </Heading>
-        <Statistics />
+      <Section>
+        <Container>
+          <Tabs>
+            <TabList>
+              <Tab>
+                <Text>Gallery</Text>
+              </Tab>
+              <Tab>
+                <Text>Todos</Text>
+              </Tab>
+            </TabList>
 
-        <Heading marginTop="50px" marginBottom="50px" textAlign="center">
-          Task 3
-        </Heading>
-        <ForbesList />
+            <TabPanel>
+              <Gallery />
+            </TabPanel>
 
-        <Heading marginTop="50px" marginBottom="50px" textAlign="center">
-          Task 4
-        </Heading>
-        <CryptoHistory />
-      </Container>
-    </Section>
+            <TabPanel>
+              <Todos />
+            </TabPanel>
+          </Tabs>
+        </Container>
+      </Section>
+    </>
   );
 };
