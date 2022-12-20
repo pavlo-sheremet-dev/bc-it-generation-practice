421a607022050aaf89f6375c29e2c1df17c52a7e
pavel-sheremet-dev <pashqa.sheremet@gmail.com>
2022-12-20 15:50:23 +0200

    add EditForm

--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -3,7 +3,39 @@ import { nanoid } from 'nanoid';
 import { Grid, GridItem, SearchForm, EditForm, Text, Todo } from 'components';
 
 export class Todos extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onclickIncrement = option => {
+    console.log(option);
+
+    this.setState(prevState => {
+      return {
+        [option]: prevState[option] + 5,
+      };
+    });
+
+    // this.setState(prevState => {
+    //   return { [option]: prevState[option] + 1 };
+    // });
+  };
+
   render() {
-    return <Text>Todos</Text>;
+    return (
+      <>
+        {Object.keys(this.state).map(option => (
+          <button
+            key={option}
+            type="button"
+            onClick={() => this.onclickIncrement(option)}
+          >
+            {option}
+          </button>
+        ))}
+      </>
+    );
   }
 }
