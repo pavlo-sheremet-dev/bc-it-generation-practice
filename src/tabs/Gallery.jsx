f8c355d061163bf81117e2118f94040ae4b9a3e6
pavel-sheremet-dev <pashqa.sheremet@gmail.com>
2022-12-21 15:52:36 +0200

    add form to gallery

--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -4,9 +4,31 @@ import * as ImageService from 'service/image-service';
 import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
 
 export class Gallery extends Component {
+  state = {
+    query: '',
+    images: [],
+  };
+
+  getQuery = queryObject => {
+    this.setState({ query: queryObject.text });
+  };
+
+  componentDidUpdate = (_, prevState) => {
+    if (prevState.query !== this.state.query) {
+      console.log('query update');
+    }
+  };
+
   render() {
+    const { images, query } = this.state;
     return (
       <>
+        <SearchForm onSubmit={this.getQuery} />
+        <p>{query}</p>
+        {!!images.length && <p>Gallery List</p>}
+        {images.length > 0 && <p>Gallery List</p>}
+        {images.length !== 0 && <p>Gallery List</p>}
+        {Boolean(images.length) && <p>Gallery List</p>}
         <Text textAlign="center">Sorry. There are no images ... 😭</Text>
       </>
     );
