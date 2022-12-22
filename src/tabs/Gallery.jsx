3ac61b66e120fbc02a2b910bc74254c82f8370e6
pavel-sheremet-dev <pashqa.sheremet@gmail.com>
2022-12-22 16:28:57 +0200

    complete services, add gallery state control logic

--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -8,38 +8,69 @@ export class Gallery extends Component {
     query: '',
     images: [],
     page: 1,
+    totalPages: 0,
+    loading: false,
+    error: '',
   };
 
   getQuery = queryObject => {
-    this.setState({ query: queryObject.text });
+    this.setState({ query: queryObject.text, images: [], page: 1 });
   };
-  componentDidMount = async () => {
-    // const { photos, totalPhotos } = await ImageService.getImages('car', 1);
-  };
+
   componentDidUpdate = async (_, prevState) => {
-    console.log('componentDidUpdate');
-    const { query, page, images } = this.state;
-    if (prevState.query !== this.state.query) {
-      const { photos, totalPhotos } = await ImageService.getImages(query, page);
-      this.setState(prevState => {
-        return { images: [...prevState.images, ...photos] };
-      });
-      // console.log(photos, totalPhotos);
+    const { query, page } = this.state;
+
+    if (prevState.query !== query || prevState.page !== page) {
+      this.getImages();
     }
   };
 
+  getImages = async () => {
+    const { query, page } = this.state;
+    this.setState({ loading: true });
+
+    try {
+      const { photos, totalPages } = await ImageService.getImages(query, page);
+
+      this.setState(prevState => {
+        return {
+          images: [...prevState.images, ...photos],
+          totalPages,
+          error: '',
+        };
+      });
+    } catch (error) {
+      this.setState({ error: 'something went wrong' });
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
+
+  incrementPage = () => {
+    this.setState(({ page }) => ({
+      page: page + 1,
+    }));
+  };
+
   render() {
-    const { images, query } = this.state;
+    const { images } = this.state;
     return (
       <>
         <SearchForm onSubmit={this.getQuery} />
-        <p>{query}</p>
-        {!!images.length && <p>Gallery List</p>}
-        {images.length > 0 && <p>Gallery List</p>}
-        {images.length !== 0 && <p>Gallery List</p>}
-        {Boolean(images.length) && <p>Gallery List</p>}
-        <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        {!!images.length && <div>TEXT</div>}
+        {images.length ? (
+          <p>Gallery List</p>
+        ) : (
+          <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        )}
+
+        {this.state.error && <p>{this.state.error}</p>}
+        <Button type="button" onClick={this.incrementPage}>
+          Load More
+        </Button>
       </>
     );
   }
 }
+
+// Кнопка не показывается когда происходит загрузка, когда последняя страница.
