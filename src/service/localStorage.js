d2e70c5a7a99c357ed6910643b62c6acfa1f1b52
pavlo-sheremet-dev <goit.pavel.sheremet@gmail.com>
2023-01-09 13:45:26 +0200

    start files

--- src/service/localStorage.js
+++
@@ -1,7 +0,0 @@
-export const getDataFromLS = (key, initialState = []) => {
-  try {
-    return JSON.parse(localStorage.getItem(key)) ?? initialState;
-  } catch (error) {
-    return initialState;
-  }
-};
--- src/styles/colors/light.js
+++
@@ -1,9 +0,0 @@
-export const light = Object.freeze({
-  accent: '#2196F3',
-  white: '#fff',
-  gray: '#9e9e9e',
-  light: '#f2f2f2',
-  dark: '#212121',
-  primary: '#49a09d',
-  mainBackground: ['linear-gradient(to top, #abbaab, #fff)'],
-});
